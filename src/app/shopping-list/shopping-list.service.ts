4ea8326df5888f3fa58c03933d47cace3a47fba6
dennis githinji <wakahiad@gmail.com>
2023-08-24 13:04:19 +0300

    adding the shopping list service, using services for ushing data from A to B

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,20 @@
+import {Ingredient} from '../shared/ingredient.model';
+import {EventEmitter} from '@angular/core';
+
 export class ShoppingListService {
-  
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  private _ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
+
+
+  get ingredients(): Ingredient[] {
+    return [...this._ingredients];
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this._ingredients.push(ingredient);
+    this.ingredientsChanged.emit([...this._ingredients]);
+  }
 }
