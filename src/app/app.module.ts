f697a422f42afcd8e065f7fb7040c200be656f4d
dennis githinji <wakahiad@gmail.com>
2023-08-28 14:14:49 +0300

    attaching the token with an interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,35 +17,40 @@ import {AppRoutingModule} from './app-routing.module';
 import {RecipeStartComponent} from './recipes/recipe-start/recipe-start.component';
 import {RecipeEditComponent} from './recipes/recipe-edit/recipe-edit.component';
 import {RecipeService} from './recipes/recipe.service';
-import {HttpClientModule} from '@angular/common/http';
-import { AuthComponent } from './auth/auth.component';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {AuthComponent} from './auth/auth.component';
 import {LoadingSpinnerComponent} from './shared/loading-spinner.component';
+import {AuthInterceptorService} from './auth/auth-interceptor.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
-    DropdownDirective,
-    RecipeStartComponent,
-    RecipeEditComponent,
-    AuthComponent,
-    LoadingSpinnerComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [ShoppingListService, RecipeService],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        RecipesComponent,
+        RecipeListComponent,
+        RecipeDetailComponent,
+        RecipeItemComponent,
+        ShoppingListComponent,
+        ShoppingEditComponent,
+        DropdownDirective,
+        RecipeStartComponent,
+        RecipeEditComponent,
+        AuthComponent,
+        LoadingSpinnerComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
+        AppRoutingModule,
+        HttpClientModule
+    ],
+    providers: [ShoppingListService, RecipeService, {
+        provide: HTTP_INTERCEPTORS,
+        useClass: AuthInterceptorService,
+        multi: true
+    }],
+    bootstrap: [AppComponent]
 })
 export class AppModule {
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -23,15 +23,13 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        return this.authService.user.pipe(take(1), exhaustMap(user => {
-            return this.http.get<Recipe[]>('https://recipe-book-bab37-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
-                {params: new HttpParams().set('auth', user.token)});
-        }), map(recipes => {
-            return recipes.map(recipe => {
-                return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
-            });
-        }), tap(recipes => {
-            this.recipesService.setRecipes(recipes);
-        }));
+        return this.http.get<Recipe[]>('https://recipe-book-bab37-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
+            .pipe(map(recipes => {
+                return recipes.map(recipe => {
+                    return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+                });
+            }), tap(recipes => {
+                this.recipesService.setRecipes(recipes);
+            }));
     }
 }
