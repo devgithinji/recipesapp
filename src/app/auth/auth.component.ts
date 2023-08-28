b6f2bfb367b1af3bcfecad86299fa2c884e48081
dennis githinji <wakahiad@gmail.com>
2023-08-28 12:20:58 +0300

    sending login requests, login error handling

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {NgForm} from '@angular/forms';
-import {AuthService} from './auth.service';
+import {AuthResponseData, AuthService} from './auth.service';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-auth',
@@ -28,19 +29,24 @@ export class AuthComponent implements OnInit {
     }
     const email = form.value.email;
     const password = form.value.password;
+
+    let authObs: Observable<AuthResponseData>;
+
     this.isLoading = true;
     if (this.isLoginMode) {
-      console.log('login');
+      authObs = this.authService.login(email, password);
     } else {
-      this.authService.signUp(email, password).subscribe(responseData => {
-        console.log(responseData);
-        this.isLoading = false;
-      }, errorMessage => {
-        console.log(errorMessage);
-        this.error = errorMessage;
-        this.isLoading = false;
-      });
+      authObs = this.authService.signUp(email, password);
     }
+
+    authObs.subscribe(responseData => {
+      console.log(responseData);
+      this.isLoading = false;
+    }, errorMessage => {
+      console.log(errorMessage);
+      this.error = errorMessage;
+      this.isLoading = false;
+    });
 
     form.reset();
   }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,43 +1,67 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {catchError} from 'rxjs/operators';
 import {throwError} from 'rxjs';
 
-interface AuthResponseData {
-  idToken: string;
-  email: string;
-  refreshToken: string;
-  expiresIn: string;
-  localId: string;
+export interface AuthResponseData {
+    idToken: string;
+    email: string;
+    refreshToken: string;
+    expiresIn: string;
+    localId: string;
+    registered?: boolean;
 }
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) {
-  }
+    constructor(private http: HttpClient) {
+    }
 
 
-  signUp(email: string, password: string) {
-    return this.http.post<AuthResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB6pyKn1XEjTKk7xCb2w7E82bg0GHdZIt8',
-      {
-        email,
-        password,
-        returnSecureToken: true
-      }
-    ).pipe(catchError(errorRes => {
-      let errorMessage = 'An unknown error occurred';
-      if (!errorRes.error || !errorRes.error.error) {
+    signUp(email: string, password: string) {
+        return this.http.post<AuthResponseData>(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB6pyKn1XEjTKk7xCb2w7E82bg0GHdZIt8',
+            {
+                email,
+                password,
+                returnSecureToken: true
+            }
+        ).pipe(catchError(this.handleError));
+    }
+
+    login(email: string, password: string) {
+        return this.http.post<AuthResponseData>(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB6pyKn1XEjTKk7xCb2w7E82bg0GHdZIt8',
+            {
+                email,
+                password,
+                returnSecureToken: true
+            }).pipe(catchError(this.handleError));
+    }
+
+
+    private handleError(errorRes: HttpErrorResponse) {
+        let errorMessage = 'An unknown error occurred';
+        if (!errorRes.error || !errorRes.error.error) {
+            return throwError(errorMessage);
+        }
+        switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+                errorMessage = 'This email exists already';
+                break;
+            case 'EMAIL_NOT_FOUND':
+                errorMessage = 'This email does not exist';
+                break;
+            case 'INVALID_PASSWORD':
+                errorMessage = 'invalid password';
+                break;
+            case 'USER_DISABLED':
+                errorMessage = 'user disabled';
+                break;
+        }
         return throwError(errorMessage);
-      }
-      switch (errorRes.error.error.message) {
-        case 'EMAIL_EXISTS':
-          errorMessage = 'This email exists already';
-      }
-      return throwError(errorMessage);
-    }));
-  }
+    }
 }
